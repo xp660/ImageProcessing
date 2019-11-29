c84feb796c32a6e3a95e145a3820e8ac53a0654d
xp660 <imxp660@gmail.com>
2019-11-29 18:59:43 +0800

    create button

--- ImageProcessing/ImageProcessing/Form1.Designer.cs
+++ ImageProcessing/ImageProcessing/Form1.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -36,19 +42,98 @@
             this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button1.Location = new System.Drawing.Point(74, 148);
+            this.button1.Location = new System.Drawing.Point(12, 217);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(105, 50);
+            this.button1.Size = new System.Drawing.Size(308, 50);
             this.button1.TabIndex = 0;
             this.button1.Text = "Load";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button2.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(308, 64);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "灰階化";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(163, 82);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(157, 77);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "反轉顏色";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.button4.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button4.Location = new System.Drawing.Point(12, 165);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 46);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "紅色濾鏡";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Lime;
+            this.button5.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button5.Location = new System.Drawing.Point(113, 165);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(95, 46);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "綠色濾鏡";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button6.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button6.Location = new System.Drawing.Point(214, 165);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(106, 46);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "藍色濾鏡";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button7.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button7.Location = new System.Drawing.Point(12, 82);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(145, 77);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "增加亮度";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(329, 278);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -59,6 +144,12 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- ImageProcessing/ImageProcessing/Form1.cs
+++ ImageProcessing/ImageProcessing/Form1.cs
@@ -8,18 +8,69 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace ImageProcessing
 {
     public partial class Form1 : Form
     {
+        ImageForm CurrentImage = null;
         public Form1()
         {
             InitializeComponent();
         }
 
+        // Load 按鈕事件處理函式
         private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory= @"C:\Users\xp660\Desktop";
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                ImageForm MyImage = new ImageForm(openFileDialog1.FileName); // 建立秀圖物件
+                MyImage.Show(); // 顯示秀圖照片
+                CurrentImage = MyImage;
+            }
+        }
+
+        // 一般處理方式: 灰階化範例
+        private void button2_Click(object sender, EventArgs e)
         {
 
         }
+
+        // 高效率影像處理 --  反轉顏色範例
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // 高效率影像處理 --  紅色濾鏡範例
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // 高效率影像處理 --  綠色濾鏡範例
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // 高效率影像處理範例 --  藍色濾鏡範例
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // 高效率影像處理範例 --  增加亮度
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
     }
+
+
+
 }
